8aa4d19f4f579bacfee3d87ae9904310e0fb8d23
ShadrackMwema <mutinda.shadrack20@gmail.com>
2024-05-12 00:59:11 +0300

    Updating Readme

--- ASSIGNMENT_10thMay/Task_1/target/debug/Task_1.d
+++ ASSIGNMENT_10thMay/Task_1/target/debug/Task_1.d
@@ -1,1 +1,1 @@
-/mnt/c/Users/HomePC/Documents/RUST/ASSIGNMENT_10thMay/Task_1/target/debug/Task_1: /mnt/c/Users/HomePC/Documents/RUST/ASSIGNMENT_10thMay/Task_1/src/main.rs
+/mnt/c/Users/HomePC/RustroverProjects/RUST/ASSIGNMENT_10thMay/Task_1/target/debug/Task_1: /mnt/c/Users/HomePC/RustroverProjects/RUST/ASSIGNMENT_10thMay/Task_1/src/main.rs
--- ASSIGNMENT_10thMay/Task_1/target/debug/deps/Task_1-bcc8aa26ba88815b.d
+++ ASSIGNMENT_10thMay/Task_1/target/debug/deps/Task_1-bcc8aa26ba88815b.d
@@ -1,5 +1,5 @@
-/mnt/c/Users/HomePC/Documents/RUST/ASSIGNMENT_10thMay/Task_1/target/debug/deps/Task_1-bcc8aa26ba88815b: src/main.rs
+/mnt/c/Users/HomePC/RustroverProjects/RUST/ASSIGNMENT_10thMay/Task_1/target/debug/deps/Task_1-bcc8aa26ba88815b: src/main.rs
 
-/mnt/c/Users/HomePC/Documents/RUST/ASSIGNMENT_10thMay/Task_1/target/debug/deps/Task_1-bcc8aa26ba88815b.d: src/main.rs
+/mnt/c/Users/HomePC/RustroverProjects/RUST/ASSIGNMENT_10thMay/Task_1/target/debug/deps/Task_1-bcc8aa26ba88815b.d: src/main.rs
 
 src/main.rs:
